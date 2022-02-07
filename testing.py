23af4f3e7beaf465b202629da2728eae25c71dc6
antosalviso <97959752+antosalviso@users.noreply.github.com>
2022-02-07 15:57:15 +0100

    Update Points

--- testing.py
+++ testing.py
@@ -1,21 +1,18 @@
 # examples/python/visualization/non_blocking_visualization.py
 
+from time import sleep
 import open3d as o3d
 import numpy as np
 import copy
 
-##Generate two random points
-randomPoints1 = np.random.rand(2, 3)
+class test:
 
-pointSet1 = o3d.geometry.PointCloud()
+    plotting=np.random.rand(200, 3)
+    pointSet= o3d.geometry.PointCloud()
 
-pointSet1.points = o3d.utility.Vector3dVector(randomPoints1)
-
-o3d.visualization.draw_geometries([pointSet1])
-
-##Add more random points
-randomPoints2 = np.random.rand(5, 3)
-
-pointSet2 = o3d.geometry.PointCloud()
-
-pointSet2.points = o3d.utility.Vector3dVector(randomPoints2)
+    for i in range(2):
+        arr=np.random.rand(200, 3)
+        plotting=np.concatenate((plotting,arr))
+        print(plotting)
+        pointSet.points = o3d.utility.Vector3dVector(plotting)
+        o3d.visualization.draw_geometries([pointSet])
