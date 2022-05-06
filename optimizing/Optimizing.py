7c9187d67b0ba173dea71380bd5831d9e48c8a58
Hugo Pettersson <51755959+HugPet@users.noreply.github.com>
2022-05-06 10:15:54 +0200

    change to np array

--- optimizing/Optimizing.py
+++ optimizing/Optimizing.py
@@ -4,8 +4,8 @@ import numpy as np
 class Optimizing():
     
     def __init__(self, points: list):
-        self.points = np.array([[points[i]["x"], points[i]["y"], points[i]["z"]] for i in range(len(points))])
-
+        self.points = points
+        
     def optimize_data(self, ratio=0.5, neighbors=20):
         # creates a points cloud in open3d
         point_cloud = o3d.geometry.PointCloud()
@@ -21,10 +21,7 @@ class Optimizing():
         removed_data = np.asarray(inlier_cloud.points)
         
         # restore data to original shape
-        data = []
         print('removed', len(self.points) - len(removed_data))
-        for i in range(len(removed_data)):
-            data.append({"x": removed_data[i][0], "y": removed_data[i][1], "z": removed_data[i][2]})
 
         # return data
-        return data
+        return removed_data
--- visualization/vizPointCloud.py
+++ visualization/vizPointCloud.py
@@ -5,7 +5,7 @@ class VisualizationPointCloud:
     
     def __init__(self, name: str, points: list) -> None:
         self._name = name; 
-        self.points = np.array([[points[i]["x"], points[i]["y"], points[i]["z"]] for i in range(len(points))])
+        self.points = points
         
     
     def get_name(self) -> str: 
