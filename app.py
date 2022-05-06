ba1734ebd3eb9613dab8924afc13298924dcb6fd
JohanOlsson99 <51018314+JohanOlsson99@users.noreply.github.com>
2022-05-06 11:40:57 +0200

    Added radius outlier and small tuning

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 import requests
 import json
 from optimizing.Optimizing import Optimizing
@@ -23,14 +21,24 @@ class App:
             self.points[i]["z"]] 
             for i in range(len(self.points))])
 
-        
-        result = input('Optimize [y/n]')
-        if result == 'y':
-            self.points = Optimizing(self.points).optimize_data(ratio=0.2, neighbors=10)
+        if (input('Do you want to optimize the point cloud? (y/n)') == 'y'):
+            self.should_optimize()
             
         self.visualization = self._select_visualization()
         self.visualization.visualize()
-            
+
+    def should_optimize(self):
+        optimizing = Optimizing(self.points)
+        while (True):
+            result = input('Optimize point cloud with: \n[1] Statistical outlier \n[2] Radius outlier \n[3] Exit\n')
+            if result == '1':
+                self.points = optimizing.statistical_outlier(ratio=0.2, neighbors=10)
+            elif result == '2':
+                self.points = optimizing.radius_outlier(nb_points=12, radius=0.10)
+            elif result == '3' or result == 'exit':
+                self.points = optimizing.get_points()
+                break
+
     def _request_session_names(self) -> str:
         """Requests all session names from point-service and creates a string from the sessions.
         Then it request int input which is represented as a specific session name.
--- optimizing/Optimizing.py
+++ optimizing/Optimizing.py
@@ -1,27 +1,41 @@
+from os import remove
 import open3d as o3d
 import numpy as np
 
 class Optimizing():
     
-    def __init__(self, points: list):
+    def __init__(self, points: np.array):
         self.points = points
         
-    def optimize_data(self, ratio=0.5, neighbors=20):
-        # creates a points cloud in open3d
-        point_cloud = o3d.geometry.PointCloud()
-        point_cloud.points = o3d.utility.Vector3dVector(self.points)
-        voxel_down_pcd = point_cloud.voxel_down_sample(voxel_size=0.02)
-        
+        # insatiate open3d
+        self.point_cloud = o3d.geometry.PointCloud()
+        self.point_cloud.points = o3d.utility.Vector3dVector(self.points)
+        self.voxel_down_pcd = self.point_cloud.voxel_down_sample(voxel_size=0.02)
+
+    def statistical_outlier(self, ratio=0.2, neighbors=10):
         # get the index for what points to keep
-        cl, ind = voxel_down_pcd.remove_statistical_outlier(nb_neighbors=neighbors,
+        cl, index = self.voxel_down_pcd.remove_statistical_outlier(nb_neighbors=neighbors,
                                                     std_ratio=ratio, print_progress=True)
+        
+        
+        self.get_point_from_index(index)
+        
+    def radius_outlier(self, nb_points=10, radius=0.1):
+        cl, index = self.voxel_down_pcd.remove_radius_outlier(nb_points=nb_points, radius=radius)
+        
+        self.get_point_from_index(index)
+
+    def get_point_from_index(self, index):
         # only gives the specific points by index
-        inlier_cloud = voxel_down_pcd.select_by_index(ind)
+        inlier_cloud = self.voxel_down_pcd.select_by_index(index)
         # create a numpy array from the chosen points
         removed_data = np.asarray(inlier_cloud.points)
         
         # restore data to original shape
-        print('removed', len(self.points) - len(removed_data))
+        print('Removed', str(len(self.points) - len(removed_data)), '\nPoints left: ' + str(len(removed_data)))
 
-        # return data
-        return removed_data
+        self.points = removed_data
+        self.point_cloud.points = o3d.utility.Vector3dVector(self.points)
+    
+    def get_points(self):
+        return self.points
