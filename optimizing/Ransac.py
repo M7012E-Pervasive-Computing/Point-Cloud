221538af79b1fc4de1dbf304d673561fd4c118c3
Denne213 <dennist1@live.se>
2022-05-31 20:02:33 +0200

    fix

--- optimizing/Ransac.py
+++ optimizing/Ransac.py
@@ -12,18 +12,18 @@ class Ransac():
         point_cloud.points = o3d.utility.Vector3dVector(self.points)
         model, inliers = point_cloud.segment_plane(distance_threshold=0.01,
                                          ransac_n=4,
-                                         num_iterations=100000)
+                                         num_iterations=1000)
         
         [a, b, c, d] = model
         print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
 
         inlier_cloud = point_cloud.select_by_index(inliers)
-        inlier_cloud.paint_uniform_color([0, 0, 1.0])
+        inlier_cloud.paint_uniform_color([1.0, 0, 0])
         outlier_cloud = point_cloud.select_by_index(inliers, invert=True)
-        outlier_cloud.paint_uniform_color([1.0, 0, 0])
+        outlier_cloud.paint_uniform_color([0, 0, 1.0])
         
-        print("Removed points: " + str((np.asarray(outlier_cloud.points)).size))
-        print("Points left: " + str(np.asarray(inlier_cloud.points).size))
+        print(f"Removed points: {(np.asarray(inlier_cloud.points)).size}")
+        print(f"Points left: {(np.asarray(outlier_cloud.points)).size}")
         
         o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud],
                                         zoom=0.8,
