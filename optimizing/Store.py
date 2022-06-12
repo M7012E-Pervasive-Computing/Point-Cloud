94dd7941fafafbb98ef2f4af7bd2a72a6901ac99
Denne213 <dennist1@live.se>
2022-06-12 17:23:48 +0200

    updated Store

--- optimizing/Store.py
+++ optimizing/Store.py
@@ -1,12 +1,9 @@
 class Store():
     def storeRoom(self, vertices, faces):
         name = input("Enter file name: ")
-        f = open(name + ".obj", "w")
-        for i in range(len(vertices)):
-            f.write("v " + str(vertices[i][0]) + " " + str(vertices[i][1]) + " " + str(vertices[i][2]) + " 1.0" + "\n")
-        for i in range(len(faces)):
-            f.write("f ")
-            for j in range(len(faces[i])):
-                f.write(str(faces[i][j]) + " ")
-            f.write("\n")
-        f.close()
+        file = open(f"models/{name}.obj", "w")
+        for v in vertices:
+            file.write(f"v {v[0]} {v[1]} {v[2]} 1.0\n")
+        for f in faces:
+            file.write(f"f {f[0]} {f[1]} {f[2]} {f[3]}\n")
+        file.close()
