c64a9163e50d7f51d6fbc92d0157cb6a54cceea9
Denne213 <65536372+Denne213@users.noreply.github.com>
2022-06-13 22:40:25 +0200

    updated app for point imports

--- Import/ImportSession.py
+++ Import/ImportSession.py
@@ -1,8 +1,6 @@
 import requests
 import json
 
-from Log.Logging import Log
-
 class ImportSession():
 
     @staticmethod
@@ -10,14 +8,17 @@ class ImportSession():
         request = requests.get('http://130.240.202.87:3000/names')
         sessions = json.loads(request.text)['sessionNames']
         if (len(sessions) == 0):
-            Log.error("No sessions available")
+            print("No sessions available")
             exit()
-        return [session['sessionName'] for session in sessions] 
+        else:
+            print("Successfully imported session names from database")
+            return [session['sessionName'] for session in sessions] 
 
     @staticmethod
     def session_points(session_name: str) -> list:
         request = requests.get('http://130.240.202.87:3000/' + session_name)
         points = json.loads(request.text)['points']
-        points = [[y, x, z] for x, y, z in points]
+        points = [[point['y'], point['x'], point['z']] for point in points]
+        print(f"Successfully imported points from session: {session_name}")
         return points
     
--- app.py
+++ app.py
@@ -1,35 +1,32 @@
 
-from anyio import get_running_tasks
-from Log.Logging import Log
 from Import.ImportSession import ImportSession as Import
 
 class App():
     
     def __init__(self) -> None:
         session_names = Import.session_names()
-        session_name = _select_session(session_names=session_names)
+        session_name = self._select_session(session_names=session_names)
         points = Import.session_points(session_name=session_name)
         
-@staticmethod
-def _select_session(session_names: list) -> str:
-        sessions_str = 'Pick a session:\n'
-        for i, session in enumerate(session_names):
-            sessions_str += f"[{i}] {session}\n"
-        session_idx = _get_int_input(len(session_names), sessions_str)
-        return session_names[session_idx]        
+    def _select_session(self, session_names: list) -> str:
+            sessions_str = 'Pick a session:\n'
+            for i, session in enumerate(session_names):
+                sessions_str += f"[{i}] {session}\n"
+            session_idx = self._get_int_input(len(session_names)-1, sessions_str)
+            return session_names[session_idx]        
 
-@staticmethod
-def _get_int_input(max: int, print_str: str = '') -> int: 
-    result = 0
-    while True:
-        try:
-            result = int(input(print_str))
-            if result > max or result < 1: 
-                Log.warn('Not a valid index')
-                continue
-            break
-        except ValueError:
-            Log.warn('Not a valid input')
-    return result
+    @staticmethod
+    def _get_int_input(max: int, print_str: str = '') -> int: 
+        result = 0
+        while True:
+            try:
+                result = int(input(print_str))
+                if result > max or result < 0: 
+                    print('Not a valid index')
+                    continue
+                break
+            except ValueError:
+                print('Not a valid input')
+        return result
 
 App()
